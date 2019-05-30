71f005063314972b4dda08f173d2634940a19bc8
jaskeerat789 <jaskee789@gmail.com>
2019-05-30 08:52:46 +0530

    init

---
+++ app.py
@@ -0,0 +1,53 @@
+import requests
+import time
+import math
+
+class EACESS():
+
+    username=None
+    header={
+        "Host":"172.31.1.6:8090",
+        "Accept": "*/*",
+        "Accept-Language":"en-US,en;q=0.5",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Referer":"https://172.31.1.6:8090/",
+        "Content-Type":"application/x-www-form-urlencoded",
+        "Content-Length": "79",
+        "DNT":"1",
+        "Connection":"keep-alive",
+        }
+
+    def get_time(self):
+        return(math.floor(time.time()*1000))
+
+    def login(self,username,password): 
+        EACESS.username=username
+        EACESS.header["content-lenght"]="79"
+        loginData={
+            "mode":191,
+            "username":username,
+            "password":password,
+            "a":self.get_time(),
+            "producttype":0
+        }
+        
+        r=requests.post('https://172.31.1.6:8090/login.xml',data=loginData,verify=False,headers=EACESS.header)
+        print(r.text)
+
+    def logout(self,username=None):
+        if username is None:
+                username=EACESS.username
+        print(username)
+        logoutData={
+            "mode":193,
+            "username":username,
+            "a":self.get_time(),
+            "producttype":0
+        }
+        EACESS.header["content-lenght"]="57"
+        r=requests.post("https://172.31.1.6:8090/logout.xml",data=logoutData,verify=False,headers=EACESS.header)
+        print(r.text)
+
+obj=EACESS()
+obj.login("username","password")      #pass your credentials in place of username(integer) and "password"
+obj.logout()
